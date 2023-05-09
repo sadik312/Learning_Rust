8c08bc3b1c3e07332af9e0f83c9fb35b819bd3bf
sadik312 <sadikislam46@gmail.com>
2023-05-09 21:50:03 +0100

    created secret key that the user will be guessing

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,7 +1,11 @@
 use std::io; // import library io from standard library (std)
+use rand::Rng; // import library rand from std (used for random numbers)
 
 fn main() { // fn declares new function called main
     println!("Guess the number!"); // prints to screen
+    
+    let secret_number = rand::thread_rng().gen_range(1, 101); // gen_range generates a random number between 1 and 100, and assigns it to secret_number
+    println!("The secret is: {}", secret_number);
     println!("Please enter your guess: "); // prints to screen
     let mut guess = String::new(); // creating mutable variable called guess (vars are immutable by default)
     // let guess = String::new(); // immutable
